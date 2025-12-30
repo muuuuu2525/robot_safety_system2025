4a025a12626af46e3a33e66b9cf8ebbff45575d9
muuuuu2525 <sheremane25@gmail.com>
2025-12-30 16:01:58 +0900

    Fix description length in setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
-import os
 from glob import glob
-from setuptools import find_packages, setup
+import os
+from setuptools import setup
 
 package_name = 'robot_safety_system'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py*'))),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.py*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='asano',
-    maintainer_email='muuuuu2525@users.noreply.github.com',
+    maintainer='Taro Chiba',
+    maintainer_email='s24c1008ae@s.chibakoudai.jp',
     description='Robot safety system.',
     license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'sensor = robot_safety_system.sensor_simulator:main',
