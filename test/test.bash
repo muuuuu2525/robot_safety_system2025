be8560bec8085c5193a22a6978b9e195dce6a208
muuuuu2525 <Sheremane25@gmail.com>
2026-01-06 14:04:04 +0900

    Fix test script: Source ROS 2 setup.bash explicitly

--- test/test.bash
+++ test/test.bash
@@ -2,6 +2,12 @@
 # SPDX-FileCopyrightText: 2025 Taro Chiba
 # SPDX-License-Identifier: Apache-2.0
 
+if [ -f /opt/ros/jazzy/setup.bash ]; then
+    source /opt/ros/jazzy/setup.bash
+elif [ -f /opt/ros/humble/setup.bash ]; then
+    source /opt/ros/humble/setup.bash
+fi
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -9,10 +15,8 @@ cd $dir/ros2_ws
 colcon build --symlink-install
 source install/setup.bash
 
-# ノードを10秒間だけ起動して、出力をログファイルに保存
 timeout 10 ros2 launch robot_safety_system safety_system_launch.py > /tmp/robot_safety.log
 
-# ログファイルの中身をチェック
 cat /tmp/robot_safety.log | grep 'Publishing distance:'
 if [ $? -ne 0 ]; then
     echo "Test Failed: Sensor data not found"
